b4727eafda7f28a44fd8bfd4d7caef142fa187b6
Loi Tran <datloiboi@gmail.com>
2023-06-13 14:00:05 -0400

    Asset query with default fields and single period column works one DB hit

--- DataAccess/Context/RSEContext.cs
+++ DataAccess/Context/RSEContext.cs
@@ -1,5 +1,6 @@
-using Microsoft.EntityFrameworkCore;
 using DataAccess.Entities;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Context
 {
@@ -8,10 +9,23 @@ namespace DataAccess.Context
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Asset> Assets { get; set; }
         public DbSet<Portfolio> Portfolios { get; set; }
-
-        
         public RseContext(DbContextOptions<RseContext> options) : base(options)
         {
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            var decimalProperties = typeof(Asset)
+                .GetProperties()
+                .Where(p => p.PropertyType == typeof(decimal));
+
+            foreach (var property in decimalProperties)
+            {
+                modelBuilder.Entity<Asset>()
+                    .Property<decimal>(property.Name)
+                    .HasColumnType("decimal(18, 2)");
+            }
         }
     }
 }
--- DataAccess/Repositories/AssetRepository.cs
+++ DataAccess/Repositories/AssetRepository.cs
@@ -1,11 +1,7 @@
 using AutoMapper;
-using System.Linq;
-using System.Linq.Dynamic.Core;
-
 using DataAccess.Context;
 using DataAccess.Entities;
 using Common.Models;
-
 
 namespace DataAccess.Repositories
 {
@@ -25,7 +21,7 @@ namespace DataAccess.Repositories
             var assets = _context.Assets;
             return assets;
         }
-        
+
         public Asset GetById(int id, string period = null)
         {
             var assetQuery = _context.Assets.Where(p => p.Id == id);
@@ -34,53 +30,39 @@ namespace DataAccess.Repositories
                 return null;
             }
 
-            var selectedProperties = new List<string> { "O", "Mc", "Pe", "Dy", "Av", "Sym", "HiDay", "LoDay", "CompanyId", "HiYear", "LoYear" };
-            var assetProperties = new List<string> { "OneDay", "OneWeek", "OneMonth", "ThreeMonths", "OneYear", "YearToDate", "AllData" };
+            Asset asset;
 
-            if (assetQuery == null)
+            switch (period)
             {
-                return null;
+                case "live":
+                    asset = assetQuery.Select(a => new Asset { Live = a.Live, O = a.O, Mc = a.Mc, Pe = a.Pe, Dy = a.Dy, Av = a.Av, Sym = a.Sym, HiDay = a.HiDay, LoDay = a.LoDay, CompanyId = a.CompanyId, HiYear = a.HiYear, LoYear = a.LoYear }).FirstOrDefault();
+                    break;
+                case "1d":
+                    asset = assetQuery.Select(a => new Asset { OneDay = a.OneDay, O = a.O, Mc = a.Mc, Pe = a.Pe, Dy = a.Dy, Av = a.Av, Sym = a.Sym, HiDay = a.HiDay, LoDay = a.LoDay, CompanyId = a.CompanyId, HiYear = a.HiYear, LoYear = a.LoYear }).FirstOrDefault();
+                    break;
+                case "1w":
+                    asset = assetQuery.Select(a => new Asset { OneWeek = a.OneWeek, O = a.O, Mc = a.Mc, Pe = a.Pe, Dy = a.Dy, Av = a.Av, Sym = a.Sym, HiDay = a.HiDay, LoDay = a.LoDay, CompanyId = a.CompanyId, HiYear = a.HiYear, LoYear = a.LoYear }).FirstOrDefault();
+                    break;
+                case "1m":
+                    asset = assetQuery.Select(a => new Asset { OneMonth = a.OneMonth, O = a.O, Mc = a.Mc, Pe = a.Pe, Dy = a.Dy, Av = a.Av, Sym = a.Sym, HiDay = a.HiDay, LoDay = a.LoDay, CompanyId = a.CompanyId, HiYear = a.HiYear, LoYear = a.LoYear }).FirstOrDefault();
+                    break;
+                case "3m":
+                    asset = assetQuery.Select(a => new Asset { ThreeMonths = a.ThreeMonths, O = a.O, Mc = a.Mc, Pe = a.Pe, Dy = a.Dy, Av = a.Av, Sym = a.Sym, HiDay = a.HiDay, LoDay = a.LoDay, CompanyId = a.CompanyId, HiYear = a.HiYear, LoYear = a.LoYear }).FirstOrDefault();
+                    break;
+                case "1y":
+                    asset = assetQuery.Select(a => new Asset { OneYear = a.OneYear, O = a.O, Mc = a.Mc, Pe = a.Pe, Dy = a.Dy, Av = a.Av, Sym = a.Sym, HiDay = a.HiDay, LoDay = a.LoDay, CompanyId = a.CompanyId, HiYear = a.HiYear, LoYear = a.LoYear }).FirstOrDefault();
+                    break;
+                case "ytd":
+                    asset = assetQuery.Select(a => new Asset { YearToDate = a.YearToDate, O = a.O, Mc = a.Mc, Pe = a.Pe, Dy = a.Dy, Av = a.Av, Sym = a.Sym, HiDay = a.HiDay, LoDay = a.LoDay, CompanyId = a.CompanyId, HiYear = a.HiYear, LoYear = a.LoYear }).FirstOrDefault();
+                    break;
+                case "all":
+                    asset = assetQuery.Select(a => new Asset { AllData = a.AllData, O = a.O, Mc = a.Mc, Pe = a.Pe, Dy = a.Dy, Av = a.Av, Sym = a.Sym, HiDay = a.HiDay, LoDay = a.LoDay, CompanyId = a.CompanyId, HiYear = a.HiYear, LoYear = a.LoYear }).FirstOrDefault();
+                    break;
+                default:
+                    return null;
             }
-
-            var asset = new Asset();
-
-            foreach (var property in selectedProperties)
-            {
-                var propertyInfo = typeof(Asset).GetProperty(property);
-                if (propertyInfo != null)
-                {
-                    var value = propertyInfo.GetValue(assetQuery.FirstOrDefault());
-                    propertyInfo.SetValue(asset, value);
-                }
-            }
-
-            var periodPropertyMap = new Dictionary<string, string>()
-            {
-                { "live", "OneDay" },
-                { "1d", "OneDay" },
-                { "1w", "OneWeek" },
-                { "1m", "OneMonth" },
-                { "3m", "ThreeMonths" },
-                { "1y", "OneYear" },
-                { "ytd", "YearToDate" },
-                { "all", "AllData" }
-            };
-
-            if (periodPropertyMap.TryGetValue(period, out var periodProperty))
-            {
-                var periodPropertyInfo = typeof(Asset).GetProperty(periodProperty);
-                if (periodPropertyInfo != null)
-                {
-                    var periodValue = periodPropertyInfo.GetValue(assetQuery.FirstOrDefault());
-                    periodPropertyInfo.SetValue(asset, periodValue);
-                }
-            }
-
             return asset;
-
         }
-
-
 
         public Asset Add(AssetDTO assetDto)
         {
